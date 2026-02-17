c815cacdf3ba43c2282f89d494b8247115a24532
rahulseervi062 <rahulseervi062@gmail.com>
2026-02-17 15:57:39 +0530

    UC3 completed using String.join()

--- Banner.java
+++ Banner.java
@@ -1,5 +1,15 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+ public class Banner
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println(" *****    *****    ******   ****** ");
+        System.out.println("*     *  *     *   *     *  *     *");
+        System.out.println("*     *  *     *   *     *  *      ");
+        System.out.println("*     *  *     *   ******   ****** ");
+        System.out.println("*     *  *     *   *        *      ");
+        System.out.println("*     *  *     *   *        *      ");
+        System.out.println(" *****    *****    *        ****** ");
+
     }
 }
